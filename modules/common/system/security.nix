c02b687461868797d25b0208c3513375fc941487
CnTeng <me@snakepi.xyz>
2022-11-11 15:20:14 +0800

    refactor: change ssh files and configs

--- modules/common/system/security.nix
+++ modules/common/system/security.nix
@@ -9,48 +9,7 @@
   environment.systemPackages = [
     agenix.defaultPackage.x86_64-linux
   ];
-  age = {
-    identityPaths = [
-      "/home/${user}/OneDrive/Backups/Agenix/agenix_ed25519"
-    ];
-    secrets = {
-      rxtx_hostname = {
-        file = ../../../secrets/rxtx_hostname.age;
-        owner = "${user}";
-        group = "users";
-        mode = "777";
-      };
-      rxtx_ed25519 = {
-        file = ../../../secrets/rxtx_ed25519.age;
-        owner = "${user}";
-        group = "users";
-        mode = "600";
-      };
-      github_auth_ed25519 = {
-        file = ../../../secrets/github_auth_ed25519.age;
-        owner = "${user}";
-        group = "users";
-        mode = "600";
-      };
-      "vaultwarden.env" = {
-        file = ../../../secrets/vaultwarden.env.age;
-        path = "/var/lib/vaultwarden.env";
-        owner = "vaultwarden";
-        group = "vaultwarden";
-        mode = "777";
-      };
-      Caddyfile = {
-        file = ../../../secrets/Caddyfile.age;
-        owner = "${user}";
-        group = "users";
-        mode = "444";
-      };
-      naiveconfig = {
-        file = ../../../secrets/naiveconfig.age;
-        owner = "${user}";
-        group = "users";
-        mode = "444";
-      };
-    };
-  };
+  age.identityPaths = [
+    "/home/${user}/OneDrive/Backups/Agenix/agenix_ed25519"
+  ];
 }
--- modules/server/caddy.nix
+++ modules/server/caddy.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }:
+{ pkgs, config, user, ... }:
 
 {
   services.caddy = {
@@ -6,11 +6,18 @@
     package = pkgs.caddy-with-plugins;
     logDir = "/var/log/caddy";
     adapter = "caddyfile";
-    configFile = config.age.secrets.Caddyfile.path;
+    configFile = config.age.secrets.caddyFile.path;
   };
 
   systemd.services.caddy.serviceConfig = {
     AmbientCapabilities = "cap_net_bind_service";
     CapabilityBoundingSet = "cap_net_bind_service";
+  };
+
+  age.secrets.caddyFile = {
+    file = ../../secrets/server/caddyFile.age;
+    owner = "${user}";
+    group = "users";
+    mode = "644";
   };
 }
