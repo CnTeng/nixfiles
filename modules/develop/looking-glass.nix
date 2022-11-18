833611f175ccba48f805471e02f0edd22a6f467e
CnTeng <me@snakepi.xyz>
2022-11-18 15:00:56 +0800

    refactor: looking-glass

---
+++ modules/develop/looking-glass.nix
@@ -0,0 +1,49 @@
+{ pkgs, user, ... }:
+
+{
+  # Enable kvmfr
+  boot.extraModulePackages = with pkgs; [
+    linuxKernel.packages.linux_6_0.kvmfr
+  ];
+
+  boot.kernelModules = [ "kvmfr" ];
+
+  boot.kernelParams = [ "kvmfr.static_size_mb=64" ];
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="kvmfr", OWNER="${user}", GROUP="kvm", MODE="0660"
+  '';
+
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+    cgroup_device_acl = [
+      "/dev/null", "/dev/full", "/dev/zero",
+      "/dev/random", "/dev/urandom",
+      "/dev/ptmx", "/dev/kvm",
+      "/dev/kvmfr0"
+    ]
+  ''; # If enable nvidia passthrough need to add nvram
+
+  # Set looking-glass to use ivshmen without kvmfr
+  # systemd.tmpfiles.rules = [
+  #   "f /dev/shm/looking-glass 0660 ${user} qemu-libvirtd -"
+  # ];
+
+  home-manager.users.${user} = {
+    programs.looking-glass-client = {
+      enable = true;
+      settings = {
+        app = {
+          shmFile = "/dev/kvmfr0";
+        };
+        win = {
+          # fullScreen = true;
+          showFPS = true;
+        };
+        spice = {
+          enable = false;
+        };
+      };
+    };
+  };
+
+}
