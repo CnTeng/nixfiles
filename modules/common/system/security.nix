f631029b4ecdfdb281a9af0cbb1c5c10a6cf3667
CnTeng <tengyufei@live.com>
2022-11-24 16:40:14 +0800

    refactor: rename agenix files

--- modules/common/system/security.nix
+++ modules/common/system/security.nix
@@ -10,6 +10,6 @@
     agenix.defaultPackage.x86_64-linux
   ];
   age.identityPaths = [
-    "/home/${user}/OneDrive/Backups/Agenix/agenix_ed25519"
+    "/home/${user}/OneDrive/Backups/agenix/agenix_ed25519"
   ];
 }
