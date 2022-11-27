204c151ac3bf2956cbe49f0fb10eea13311a9731
CnTeng <me@snakepi.xyz>
2022-11-27 16:15:24 +0800

    refactor: hardware

--- hosts/server/hardware.nix
+++ hosts/server/hardware.nix
@@ -4,22 +4,26 @@
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
-  boot.loader.grub.device = "/dev/vda";
+  boot = {
+    loader.grub.device = "/dev/vda";
 
-  boot.initrd.availableKernelModules = [
-    "ata_piix"
-    "uhci_hcd"
-    "xen_blkfront"
-  ];
-  boot.initrd.kernelModules = [ "nvme" ];
+    initrd = {
+      availableKernelModules = [
+        "ata_piix"
+        "uhci_hcd"
+        "xen_blkfront"
+      ];
+      kernelModules = [ "nvme" ];
+    };
 
-  boot.cleanTmpDir = true;
+    cleanTmpDir = true;
 
-  # Enable BBR module
-  boot.kernelModules = [ "tcp_bbr" ];
-  boot.kernel.sysctl = {
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "net.core.default_qdisc" = "cake";
+    # Enable BBR module
+    kernelModules = [ "tcp_bbr" ];
+    kernel.sysctl = {
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.core.default_qdisc" = "cake";
+    };
   };
 
   fileSystems."/" = {
