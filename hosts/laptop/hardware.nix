41476f4463223b62e96cd3aa9a3b40e43331b5f7
CnTeng <tengyufei@live.com>
2022-11-27 16:15:24 +0800

    refactor: hardware

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -5,33 +5,36 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [
-    "xhci_pci"
-    "ahci"
-    "nvme"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [ ];
+  boot = {
+    # Use the latest kernel
+    kernelPackages = pkgs.linuxPackages_latest;
 
-  # Use the latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
 
-  boot.kernelModules = [ ];
-
-  # Only use S3 suspend mode, but it not work
-  # boot.kernelParams = [ "acpi_rev_override=1" "acpi_osi=Linux" "mem_sleep_default=deep" ];
-  boot.extraModulePackages = [ ];
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "btrfs";
+    # Only use S3 suspend mode, but it not work
+    # kernelParams = [
+    #   "acpi_rev_override=1"
+    #   "acpi_osi=Linux"
+    #   "mem_sleep_default=deep"
+    # ];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/boot";
-    fsType = "vfat";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [{
