16a284a0b6d558755908bc3d671eb6e43fc1e5f4
CnTeng <tengyufei@live.com>
2022-11-29 20:36:12 +0800

    fix: back to home manager

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
     home-manager = {
-      url = github:CnTeng/home-manager;
+      url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
