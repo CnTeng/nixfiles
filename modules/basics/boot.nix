de161e266af4d153cbb90b92c1b2a9f7b501f2c4
CnTeng <tengyufei@live.com>
2022-12-04 17:20:24 +0800

    fix: disable plymouth

--- modules/basics/boot.nix
+++ modules/basics/boot.nix
@@ -1,27 +1,22 @@
 { ... }:
 
 {
-  boot = {
-    loader = {
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
-      systemd-boot = {
-        enable = true;
-        consoleMode = "max";
-      };
-      # grub = {
-      #   enable = true;
-      #   version = 2;
-      #   devices = [ "nodev" ];
-      #   efiSupport = true;
-      #   useOSProber = true;
-      # };
-      timeout = 1;
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
     };
-    plymouth = {
+    systemd-boot = {
       enable = true;
+      consoleMode = "max";
     };
+    # grub = {
+    #   enable = true;
+    #   version = 2;
+    #   devices = [ "nodev" ];
+    #   efiSupport = true;
+    #   useOSProber = true;
+    # };
+    timeout = 1;
   };
 }
