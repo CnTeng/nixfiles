a84e508badca4e780d491e4f5400aa407251002c
CnTeng <tengyufei@live.com>
2022-12-09 18:05:59 +0800

    refactor: console

--- modules/common/system/console.nix
+++ modules/common/system/console.nix
@@ -1,26 +1,46 @@
 { ... }:
 
+let
+  # Catppuccin Macchiato
+  # Copy from https://github.com/catppuccin/catppuccin
+  base00 = "24273a"; # base
+  base01 = "1e2030"; # mantle
+  base02 = "363a4f"; # surface0
+  base03 = "494d64"; # surface1
+  base04 = "5b6078"; # surface2
+  base05 = "cad3f5"; # text
+  base06 = "f4dbd6"; # rosewater
+  base07 = "b7bdf8"; # lavender
+  base08 = "ed8796"; # red
+  base09 = "f5a97f"; # peach
+  base0A = "eed49f"; # yellow
+  base0B = "a6da95"; # green
+  base0C = "8bd5ca"; # teal
+  base0D = "8aadf4"; # blue
+  base0E = "c6a0f6"; # mauve
+  base0F = "f0c6c6"; # flamingo
+in
 {
   console = {
     font = "Lat2-Terminus16";
     useXkbConfig = true;
     colors = [
-      "24273a" # base
-      "1e2030" # mantle
-      "363a4f" # surface0
-      "494d64" # surface1
-      "5b6078" # surface2
-      "cad3f5" # text
-      "f4dbd6" # rosewater
-      "b7bdf8" # lavender
-      "ed8796" # red
-      "f5a97f" # peach
-      "eed49f" # yellow
-      "a6da95" # green
-      "8bd5ca" # teal
-      "8aadf4" # blue
-      "c6a0f6" # mauve
-      "f0c6c6" # flamingo
+      "${base00}"
+      "${base01}"
+      "${base02}"
+      "${base03}"
+      "${base04}"
+      "${base05}"
+      "${base06}"
+      "${base07}"
+      "${base08}"
+      "${base09}"
+      "${base0A}"
+      "${base0B}"
+      "${base0C}"
+      "${base0D}"
+      "${base0E}"
+      "${base0F}"
     ];
   };
 }
