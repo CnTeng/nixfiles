3851cdc7423f323c66fe2cdc3a698a9cf27e8b6c
CnTeng <tengyufei@live.com>
2022-12-09 18:04:14 +0800

    refactor: fcitx

--- modules/basics/fonts.nix
+++ modules/basics/fonts.nix
@@ -2,49 +2,56 @@
 
 {
   fonts = {
+    fontDir.enable = true;
+
     fonts = with pkgs; [
-      dejavu_fonts # Need for looking-glass
-      # source-sans-pro
-      # source-serif-pro
-      # source-code-pro
       # source-han-sans
       # source-han-serif
       # source-han-mono
+      # source-sans-pro
+      # source-serif-pro
+      # source-code-pro
+      roboto
+      roboto-slab
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       noto-fonts-emoji
-      roboto
-      roboto-slab
-      roboto-mono
+      wqy_zenhei
       (nerdfonts.override {
-        fonts = [ "FiraCode" "RobotoMono" ];
+        fonts = [
+          "FiraCode"
+          "RobotoMono"
+          "Noto"
+        ];
       })
     ] ++ (with config;[
       nur.repos.rewine.ttf-ms-win10
     ]);
-    fontconfig = {
-      defaultFonts = {
-        serif = [
-          # "Source Serif"
-          # "Source Han Serif SC"
-          "Roboto Slab"
-          "Noto Serif CJK SC"
-        ];
-        sansSerif = [
-          # "Source Sans"
-          # "Source Han Sans SC"
-          "Roboto"
-          "Noto Sans CJK SC"
-        ];
-        monospace = [
-          # "Source Code Pro"
-          # "Source Han Mono SC"
-          "Roboto Mono"
-          "Noto Sans Mono CJK SC"
-        ];
-        emoji = [ "Noto Color Emoji" ];
-      };
+
+    fontconfig.defaultFonts = {
+      serif = [
+        # "Source Serif"
+        # "Source Han Serif SC"
+        "Roboto Slab"
+        "Noto Serif CJK SC"
+        "NotoSerif Nerd Font"
+      ];
+      sansSerif = [
+        # "Source Sans"
+        # "Source Han Sans SC"
+        "Roboto"
+        "Noto Sans CJK SC"
+        "NotoSans Nerd Font"
+      ];
+      monospace = [
+        # "Source Code Pro"
+        # "Source Han Mono SC"
+        "RobotoMono Nerd Font"
+        "Noto Sans Mono CJK SC"
+
+      ];
+      emoji = [ "Noto Color Emoji" ];
     };
   };
 
