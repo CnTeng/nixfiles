f2eeebf282adf5cb4887d0f8aedf7a710db0ebf2
CnTeng <me@snakepi.xyz>
2022-12-13 19:39:44 +0800

    fix: ssh rxtx

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -10,10 +10,10 @@
 
   users.users = {
     ${user}.openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMM41+K+kV5SHzktMpFw+Q/rpb0c73rVilhUjZPTdZy+AAAACXNzaDp5dWZlaQ== ssh:yufei"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJsesttoCaugSKmHFIORINbEhvvqa7IhKO4viq3WLYktAAAADnNzaDpyeHR4QE5peE9T ssh:rxtx@NixOS"
     ];
     root.openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMM41+K+kV5SHzktMpFw+Q/rpb0c73rVilhUjZPTdZy+AAAACXNzaDp5dWZlaQ== ssh:yufei"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIJsesttoCaugSKmHFIORINbEhvvqa7IhKO4viq3WLYktAAAADnNzaDpyeHR4QE5peE9T ssh:rxtx@NixOS"
     ];
   };
 }
