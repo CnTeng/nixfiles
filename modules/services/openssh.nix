755571873ab79f1976daffdf502f682acafdbeae
CnTeng <tengyufei@live.com>
2022-12-13 19:16:18 +0800

    fix: ssh

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -10,10 +10,10 @@
 
   users.users = {
     ${user}.openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAFcDV0fFdh2mcI/A1Q0/LRkD4gPjA1vIxtD33yEZbwbAAAABHNzaDo= rxtx@NixOS"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMM41+K+kV5SHzktMpFw+Q/rpb0c73rVilhUjZPTdZy+AAAACXNzaDp5dWZlaQ== ssh:yufei"
     ];
     root.openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIAFcDV0fFdh2mcI/A1Q0/LRkD4gPjA1vIxtD33yEZbwbAAAABHNzaDo= rxtx@NixOS"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMM41+K+kV5SHzktMpFw+Q/rpb0c73rVilhUjZPTdZy+AAAACXNzaDp5dWZlaQ== ssh:yufei"
     ];
   };
 }
