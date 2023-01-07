b6efbada132f921176e457e763fd89155b2e966c
CnTeng <istengyf@outlook.com>
2023-01-07 21:27:10 +0800

    fix: powersave

--- modules/programs/kdeconnect.nix
+++ modules/programs/kdeconnect.nix
@@ -1,6 +1,15 @@
 { pkgs, user, ... }:
 
 {
+  networking.firewall = {
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+  };
+
   programs.kdeconnect.enable = true;
   home-manager.users.${user} = {
     services.kdeconnect = {
