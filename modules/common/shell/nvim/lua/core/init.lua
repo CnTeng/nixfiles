31554d278911821903066a3999034d333e579838
CnTeng <me@snakepi.xyz>
2023-01-10 22:45:30 +0800

    feat: neovim autocmds

--- modules/common/shell/nvim/lua/core/init.lua
+++ modules/common/shell/nvim/lua/core/init.lua
@@ -1,7 +1,7 @@
 for _, source in ipairs {
 	"core.options",
 	"core.keymaps",
-	"core.autocommands",
+	"core.autocmds",
 } do
 	local status_ok, fault = pcall(require, source)
 	if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
