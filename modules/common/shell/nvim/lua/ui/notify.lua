cf67ea134012c3c077c6dacbeb477d0d6775eb58
CnTeng <me@snakepi.xyz>
2023-01-13 00:07:49 +0800

    refactor: neovim config

--- modules/common/shell/nvim/lua/ui/notify.lua
+++ modules/common/shell/nvim/lua/ui/notify.lua
@@ -1,12 +1,12 @@
-local plugin = require "utils.plugin"
-
-local notify = plugin.pcall "notify"
-if not notify then return end
-
-notify.setup {
-	fps = 60,
-	render = "minimal",
-	stages = "slide",
-	timeout = 3000,
+return {
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			fps = 60,
+			render = "minimal",
+			stages = "slide",
+			timeout = 3000,
+		},
+		config = function() vim.notify = require "notify" end,
+	},
 }
-vim.notify = notify
