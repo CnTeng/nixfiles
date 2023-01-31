4f8c34fb0e12a3adb2153d901ea0089883352433
CnTeng <istengyf@outlook.com>
2023-01-31 20:38:46 +0800

    fix: ssh port

--- modules/basics/ssh.nix
+++ modules/basics/ssh.nix
@@ -9,7 +9,6 @@
       matchBlocks.rxtx = {
         hostname = "ssh.snakepi.xyz";
         user = "yufei";
-        port = 23;
         forwardAgent = true;
         identityFile = [
           "/home/${user}/.ssh/id_ed25519_sk_rk_rxtx@NixOS"
