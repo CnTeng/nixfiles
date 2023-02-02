f1b12027aa966a6ea11d40b8e8511a47e08c94b9
CnTeng <me@snakepi.xyz>
2023-02-02 22:46:57 +0800

    refactor: neovim lsp

---
+++ lua/coding/lsp/null-ls.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    -- event = "BufReadPre",
+    dependencies = "williamboman/mason.nvim",
+    opts = function()
+      local nls = require "null-ls"
+      return {
+        sources = {
+          nls.builtins.formatting.prettier,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+        },
+      }
+    end,
+  },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    dependencies = "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- "stylua",
+        -- "prettier",
+        -- "cpplint",
+        -- "golines",
+        -- "black",
+        -- "shfmt",
+      },
+    },
+  },
+}
---
+++ lua/editor/which-key.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      key_labels = { ["<leader>"] = "SPC" },
+    },
+    config = function(_, opts)
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      --[[   t = { ]]
+      --[[     name = "Terminal", ]]
+      --[[     f = { "<cmd>ToggleTerm direction=float<cr>", "Float" }, ]]
+      --[[     h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" }, ]]
+      --[[     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" }, ]]
+      --[[   }, ]]
+      --[[ } ]]
+      --[[]]
+
+      local wk = require "which-key"
+      wk.setup(opts)
+      wk.register {
+        mode = { "n", "v" },
+        ["]"] = { name = "+Next" },
+        ["["] = { name = "+Prev" },
+        ["g"] = { name = "+Goto" },
+        ["<leader>b"] = { name = "+Buffer" },
+        ["<leader>f"] = { name = "+File" },
+        ["<leader>g"] = { name = "+Git" },
+        ["<leader>l"] = { name = "+LSP" },
+        ["<leader>p"] = { name = "+Plugin" },
+        ["<leader>s"] = { name = "+Search" },
+      }
+    end,
+  },
+}
