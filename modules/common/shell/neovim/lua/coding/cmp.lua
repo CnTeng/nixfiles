5b0c7e79ecd49e0b1bc880ba1f1aa640e550f93a
CnTeng <istengyf@outlook.com>
2023-02-03 22:26:50 +0800

    feat: neovim remove mason

--- modules/common/shell/neovim/lua/coding/cmp.lua
+++ modules/common/shell/neovim/lua/coding/cmp.lua
@@ -1,10 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    event = {
-      "InsertEnter",
-      "CmdlineEnter ",
-    },
+    event = "VeryLazy",
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
       "saadparwaiz1/cmp_luasnip",
