3b1237e2ae1886ada844d2b9d3fd766dd65c13f5
CnTeng <me@snakepi.xyz>
2023-02-07 00:16:25 +0800

    refactor: neovim config & add dap support

--- modules/common/shell/neovim/lua/editor/toggleterm.lua
+++ modules/common/shell/neovim/lua/editor/toggleterm.lua
@@ -1,33 +1,29 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		opts = {
-			size = function(term)
-				if term.direction == "horizontal" then
-					return 15
-				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.4
-				end
-			end,
-			open_mapping = [[<c-\>]],
-			hide_numbers = true,
-			shade_filetypes = {},
-			shade_terminals = true,
-			shading_factor = 2,
-			start_in_insert = true,
-			insert_mappings = true,
-			persist_size = true,
-			direction = "float",
-			close_on_exit = true,
-			--[[ shell = vim.fn.has "win32" == 1 and "pwsh -Nologo" or "fish", ]]
-			float_opts = {
-				border = "curved",
-				winblend = 0,
-				highlights = {
-					border = "Normal",
-					background = "Normal",
-				},
-			},
-		},
-	},
+  "akinsho/toggleterm.nvim",
+  cmd = "ToggleTerm",
+  keys = {
+    { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
+    { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
+    { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" },
+  },
+  opts = function()
+    return {
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      shading_factor = 2,
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
+    }
+  end,
 }
