f8b56390209c32b98704492f24e91c1aea3980bd
CnTeng <me@snakepi.xyz>
2023-02-08 23:02:51 +0800

    fix(neovim:ui): remove colorscheme custom config

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -1,82 +1,60 @@
 return {
   "catppuccin/nvim",
+  lazy = true,
   name = "catppuccin",
-  lazy = false,
-  priority = 1000,
-  opts = function()
-    local ucolors = require "catppuccin.utils.colors"
-    local cp = require("catppuccin.palettes").get_palette()
-
-    return {
-      flavour = "macchiato", -- latte, frappe, macchiato, mocha
-      styles = {
-        comments = { "italic" },
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
+  opts = {
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    styles = {
+      comments = { "italic" },
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    integrations = {
+      fidget = true,
+      gitsigns = true,
+      leap = true,
+      neotree = true,
+      cmp = true,
+      dap = {
+        enabled = true,
+        enable_ui = true,
       },
-      highlight_overrides = {
-        all = {
-          QuickFixLine = {
-            bg = ucolors.darken(cp.blue, 0.36),
-            style = { "bold" },
-          },
-          Visual = {
-            bg = ucolors.darken(cp.blue, 0.36),
-            style = { "bold" },
-          },
-          VisualNOS = {
-            bg = ucolors.darken(cp.blue, 0.36),
-            style = { "bold" },
-          },
-        },
+      notify = true,
+      treesitter_context = true,
+      treesitter = true,
+      telescope = true,
+      illuminate = true,
+      which_key = true,
+      indent_blankline = {
+        enabled = true,
+        colored_indent_levels = true,
       },
-      integrations = {
-        fidget = true,
-        gitsigns = true,
-        leap = true,
-        neotree = true,
-        cmp = true,
-        dap = {
-          enabled = true,
-          enable_ui = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
         },
-        notify = true,
-        treesitter_context = true,
-        treesitter = true,
-        telescope = true,
-        illuminate = true,
-        which_key = true,
-        indent_blankline = {
-          enabled = true,
-          colored_indent_levels = true,
-        },
-        native_lsp = {
-          enabled = true,
-          virtual_text = {
-            errors = { "italic" },
-            hints = { "italic" },
-            warnings = { "italic" },
-            information = { "italic" },
-          },
-          underlines = {
-            errors = { "underline" },
-            hints = { "underline" },
-            warnings = { "underline" },
-            information = { "underline" },
-          },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
         },
       },
-    }
-  end,
+    },
+  },
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme "catppuccin"
