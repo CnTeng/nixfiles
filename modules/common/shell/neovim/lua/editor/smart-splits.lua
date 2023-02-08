8fc8964d63c12b1554d5213832699d98b797954c
CnTeng <me@snakepi.xyz>
2023-02-08 22:59:40 +0800

    fix: neovim comment

--- modules/common/shell/neovim/lua/editor/smart-splits.lua
+++ modules/common/shell/neovim/lua/editor/smart-splits.lua
@@ -7,7 +7,7 @@ return {
     { "<A-j>", function() require("smart-splits").resize_down() end },
     { "<A-k>", function() require("smart-splits").resize_up() end },
     { "<A-l>", function() require("smart-splits").resize_right() end },
-    -- Moving between splits
+    -- Better window navigation
     { "<C-h>", function() require("smart-splits").move_cursor_left() end },
     { "<C-j>", function() require("smart-splits").move_cursor_down() end },
     { "<C-k>", function() require("smart-splits").move_cursor_up() end },
