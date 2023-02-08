e0e0e80d9da123bd35e4e8acef75e11369bf4fa5
CnTeng <istengyf@outlook.com>
2023-02-08 23:04:28 +0800

    feat(neovim:editor): finish toggleterm config

--- modules/common/shell/neovim/lua/editor/toggleterm.lua
+++ modules/common/shell/neovim/lua/editor/toggleterm.lua
@@ -5,6 +5,11 @@ return {
     { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "ToggleTerm float" },
     { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "ToggleTerm horizontal split" },
     { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "ToggleTerm vertical split" },
+    {
+      "<leader>tl",
+      function() require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true }):toggle() end,
+      desc = "Lazygit",
+    },
   },
   opts = function()
     return {
