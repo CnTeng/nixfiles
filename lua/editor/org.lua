7c68080078d8b637d0bdac1b7b096f944a29ec4d
CnTeng <me@snakepi.xyz>
2023-02-08 23:05:03 +0800

    feat(neovim:editor): org support

--- lua/editor/org.lua
+++ lua/editor/org.lua
@@ -1,10 +1,17 @@
 return {
   "nvim-orgmode/orgmode",
   ft = "org",
+  dependencies = {
+    { "akinsho/org-bullets.nvim", config = true },
+    -- { "lukas-reineke/headlines.nvim", config = true },
+  },
   init = function()
     vim.opt.conceallevel = 2
     vim.opt.concealcursor = "nc"
     vim.opt.shellslash = true
   end,
-  config = true,
+  config = function()
+    require("orgmode").setup_ts_grammar()
+    require("orgmode").setup()
+  end,
 }
