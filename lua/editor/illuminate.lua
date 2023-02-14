46fe1a4722c4343ac50614b6a9adac65810ea28c
CnTeng <me@snakepi.xyz>
2023-02-14 20:33:28 +0800

    fix(neovim): add bufnewfile lazy event

--- lua/editor/illuminate.lua
+++ lua/editor/illuminate.lua
@@ -1,7 +1,6 @@
 return {
   "RRethy/vim-illuminate",
-  event = "BufReadPost",
-  -- stylua: ignore
+  event = { "BufReadPost", "BufNewFile" },
   keys = {
     { "]]", function() require("illuminate").goto_next_reference(false) end, desc = "Next reference" },
     { "[[", function() require("illuminate").goto_prev_reference(false) end, desc = "Previous reference" },
