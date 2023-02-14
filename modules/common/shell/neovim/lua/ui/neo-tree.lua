423ae7223b3751b8acc94374b940b1bf0ab7b47b
CnTeng <me@snakepi.xyz>
2023-02-14 20:31:51 +0800

    fix(neovim): fix cmd lazy issue

--- modules/common/shell/neovim/lua/ui/neo-tree.lua
+++ modules/common/shell/neovim/lua/ui/neo-tree.lua
@@ -1,13 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v2.x",
-  event = "VeryLazy",
+  cmd = "Neotree",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  cmd = "Neotree",
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Explorer" },
   },
