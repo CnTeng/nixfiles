6031fa7023d60f295aa3a1ae7fc44b1a7345ce16
CnTeng <me@snakepi.xyz>
2023-02-14 20:33:28 +0800

    fix(neovim): add bufnewfile lazy event

--- modules/common/shell/neovim/lua/coding/treesitter.lua
+++ modules/common/shell/neovim/lua/coding/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  event = "BufReadPost",
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
     ensure_installed = {
       "vim",
@@ -32,7 +32,7 @@ return {
     autopairs = { enable = true },
     incremental_selection = { enable = true },
     indent = {
-      enable = true,
+      enable = false,
       disable = {},
     },
     context_commentstring = {
