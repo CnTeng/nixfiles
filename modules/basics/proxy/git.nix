a21d72da119aa0bbe14eda8da102b2376c287d80
CnTeng <istengyf@outlook.com>
2023-02-17 16:18:56 +0800

    feat(basics): add ssh proxy

--- modules/basics/proxy/git.nix
+++ modules/basics/proxy/git.nix
@@ -6,5 +6,9 @@
       http.proxy = "http://127.0.0.1:10809";
       https.proxy = "http://127.0.0.1:10809";
     };
+
+    programs.ssh.matchBlocks."github.com" = {
+      proxyCommand = "nc -v -x 127.0.0.1:10808 %h %p";
+    };
   };
 }
