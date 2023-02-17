9f1c0cd3ebc0306b966ae9477f01983fd6488139
CnTeng <me@snakepi.xyz>
2023-02-17 14:45:00 +0800

    feat(neovim:coding): use nix to install treesitter

--- lua/coding/treesitter.lua
+++ lua/coding/treesitter.lua
@@ -1,44 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    ensure_installed = {
-      "vim",
-      "lua",
-      "json",
-      "bash",
-      "c",
-      "cpp",
-      "go",
-      "nix",
-      "python",
-      "markdown",
-      "markdown_inline",
-      "org",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "markdown", "org" },
+      },
+      incremental_selection = { enable = true },
+      indent = { enable = false },
+      rainbow = {
+        enable = true,
+        disable = { "html" },
+      },
+      autotag = { enable = true },
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
     },
-    ignore_install = {},
-    highlight = {
-      enable = true,
-      disable = {},
-      additional_vim_regex_highlighting = { "markdown", "org" },
-    },
-    rainbow = {
-      enable = true,
-      disable = { "html" },
-      extended_mode = false,
-      max_file_lines = nil,
-    },
-    autopairs = { enable = true },
-    incremental_selection = { enable = true },
-    indent = {
-      enable = false,
-      disable = {},
-    },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
+    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
   },
-  config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
 }
