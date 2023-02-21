fabc7857f29c87853915f6da7e7372e7dfb46c30
CnTeng <istengyf@outlook.com>
2023-02-21 21:47:23 +0800

    feat(programs): add wezterm

---
+++ modules/programs/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+return {
+	underline_thickness = 3,
+	underline_position = -3,
+	font_size = 15.0,
+	font = wezterm.font("FiraCode Nerd Font"),
+
+	color_scheme = "Catppuccin Macchiato",
+}
