f0db0983a6ee91ea9603dcd8a29d49bc7bfb529e
CnTeng <me@snakepi.xyz>
2023-02-28 16:43:39 +0800

    fix: fix subtree

--- modules/shell/neovim/init.lua
+++
@@ -1,9 +0,0 @@
-require "core"
-
-require("core.utils.plugin").load {
-  { import = "ui" },
-  { import = "editor" },
-  { import = "coding" },
-  { import = "coding.lsp" },
-  { import = "coding.dap" },
-}
--- modules/shell/neovim/lua/editor/colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "NvChad/nvim-colorizer.lua",
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    user_default_options = { names = false },
-  },
-}
