8906f19fc07c82aafc6bf31fecf5095d2687994d
CnTeng <me@snakepi.xyz>
2023-03-06 10:24:29 +0800

    feat: add rxaws

---
+++ hosts/rxaws/default.nix
@@ -0,0 +1,24 @@
+_: {
+  networking.hostName = "rxtx";
+
+  imports = [
+    ./hardware.nix
+
+    ../../modules/basics
+    ../../modules/services
+    ../../modules/shell
+  ];
+
+  custom = {
+    basics.ssh.enable = false;
+    services = {
+      firewall.enable = true;
+      naive.enable = true;
+      openssh.enable = true;
+    };
+    shell = {
+      proxy.enable = false;
+      neovim.withNixTreesitter = false;
+    };
+  };
+}
