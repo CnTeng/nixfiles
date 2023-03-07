302f0d2386700ebffc4306baf7ffcb4b1b500a61
CnTeng <me@snakepi.xyz>
2023-03-07 22:20:29 +0800

    feat: add rxhz key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,13 +6,15 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjCERLilIWtMakao9loYQXAb+6I3kPHuOVVdsTANs8U rxdell";
   rxaws =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfR8ocU9mPnUG2YYhqAm8WAfkYnGkxQ3lusE/TzpvE9 rxaws";
+  rxhz =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvDCURMwDZ6Kpldv4HCZbS8bBzPD03rt6dCHX7UmK2C rxhz";
   rxtx =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPH1/9YLdEA+/NQzNzSkVlN6RWxpsgpH+Z0N53n5PZut rxtx";
 in {
   "proxy/naiveConfig.age".publicKeys = [ yufei rxdell ];
 
-  "services/naiveFile.age".publicKeys = [ yufei rxaws rxtx ];
-  "services/caddyFile.age".publicKeys = [ yufei rxaws rxtx ];
-  "services/vaultwardenEnv.age".publicKeys = [ yufei rxaws rxtx ];
-  "services/minifluxAdmin.age".publicKeys = [ yufei rxaws rxtx ];
+  "services/naiveFile.age".publicKeys = [ yufei rxaws rxhz rxtx ];
+  "services/caddyFile.age".publicKeys = [ yufei rxaws rxhz rxtx ];
+  "services/vaultwardenEnv.age".publicKeys = [ yufei rxaws rxhz rxtx ];
+  "services/minifluxAdmin.age".publicKeys = [ yufei rxaws rxhz rxtx ];
 }
