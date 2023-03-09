65183e3aa4ef1db90e43b414615702f37ca89249
CnTeng <me@snakepi.xyz>
2023-03-09 21:55:04 +0800

    refactor(services): change services about caddy

--- modules/services/calibre.nix
+++ modules/services/calibre.nix
@@ -9,20 +9,44 @@ in {
   options.custom.services.calibre-web.enable = mkEnableOption "Calibre-Web";
 
   config = mkIf cfg.enable {
-    services.calibre-web = {
-      enable = true;
-      user = "${user}";
+    services = {
+      calibre-web = {
+        enable = true;
+        user = "${user}";
+        group = "users";
+        listen = {
+          ip = "127.0.0.1";
+          port = 7222;
+        };
+        openFirewall = true;
+        options = {
+          enableBookUploading = true;
+          enableBookConversion = true;
+          calibreLibrary = "${homeDirectory}/OneDrive/Calibre";
+        };
+      };
+
+      caddy.virtualHosts."book.snakepi.xyz" = {
+        logFormat = ''
+          output file ${config.services.caddy.logDir}/book.log
+        '';
+        extraConfig = ''
+          import ${config.age.secrets.caddy.path}
+
+          bind
+
+          encode gzip
+
+          reverse_proxy 127.0.0.1:7222
+        '';
+      };
+    };
+
+    age.secrets.calibre = {
+      file = ../../secrets/services/calibre.age;
+      owner = "${user}";
       group = "users";
-      listen = {
-        ip = "127.0.0.1";
-        port = 7222;
-      };
-      openFirewall = true;
-      options = {
-        enableBookUploading = true;
-        enableBookConversion = true;
-        calibreLibrary = "${homeDirectory}/OneDrive/Calibre";
-      };
+      mode = "644";
     };
   };
 }
