0c3b48e2c090d06a616aa4d2edb63a9dda19c980
CnTeng <me@snakepi.xyz>
2023-03-26 20:17:16 +0800

    feat(shell): add bat

--- modules/shell/bat.nix
+++ modules/shell/bat.nix
@@ -12,7 +12,20 @@ in {
     home-manager.users.${user} = {
       programs.bat = {
         enable = true;
-
+        config = { theme = "Catppuccin-macchiato"; };
+        themes = mapAttrs' (name: _:
+          nameValuePair ("Catppuccin-" + name) (builtins.readFile
+            (pkgs.fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "bat";
+              rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+              sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+            } + "/Catppuccin-${name}.tmTheme"))) {
+              frappe = "frappe";
+              latte = "latte";
+              macchiato = "macchiato";
+              mocha = "mocha";
+            };
       };
     };
   };
