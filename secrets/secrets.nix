41eaf49efed4ad70d88771af207f1f67f72f9aa1
CnTeng <me@snakepi.xyz>
2023-03-26 20:15:10 +0800

    feat(sercrets): rename sercrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -8,13 +8,11 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHfR8ocU9mPnUG2YYhqAm8WAfkYnGkxQ3lusE/TzpvE9 rxaws";
   rxhz =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMvDCURMwDZ6Kpldv4HCZbS8bBzPD03rt6dCHX7UmK2C rxhz";
-  rxtx =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPH1/9YLdEA+/NQzNzSkVlN6RWxpsgpH+Z0N53n5PZut rxtx";
 in {
-  "services/caddy.age".publicKeys = [ yufei rxhz rxtx ];
-  "services/miniflux.age".publicKeys = [ yufei rxhz rxtx ];
-  "services/naive.age".publicKeys = [ yufei rxaws rxhz rxtx ];
-  "services/vaultwarden.age".publicKeys = [ yufei rxhz rxtx ];
+  "services/caddy.age".publicKeys = [ yufei rxaws rxhz ];
+  "services/miniflux.age".publicKeys = [ yufei rxhz ];
+  "services/naive.age".publicKeys = [ yufei rxaws rxhz ];
+  "services/vaultwarden.age".publicKeys = [ yufei rxhz ];
 
   "shell/naive.age".publicKeys = [ yufei rxdell ];
 }
