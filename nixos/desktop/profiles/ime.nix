0eb660344084e0d6839926ecd345ba8c4bfcc629
CnTeng <me@snakepi.xyz>
2023-09-08 01:05:35 +0800

    refactor: ime and themes

--- nixos/desktop/profiles/ime.nix
+++ nixos/desktop/profiles/ime.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   config,
   lib,
   pkgs,
@@ -7,6 +8,9 @@
 }:
 with lib; let
   cfg = config.desktop'.profiles.inputMethod;
+
+  inherit (config.basics'.colors) flavour;
+  inherit (inputs.themes) fcitx5Theme;
 in {
   options.desktop'.profiles.inputMethod.enable =
     mkEnableOption "input method component";
@@ -18,30 +22,43 @@ in {
       libinput.enable = true;
     };
 
-    i18n.inputMethod = {
-      enabled = "fcitx5";
-      fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-chinese-addons
-          fcitx5-pinyin-zhwiki
-        ];
-        settings = {
-          addons = {
-            classicui.globalSection.Theme = "catppuccin-mocha";
+    home-manager.users.${user} = {
+      i18n.inputMethod = {
+        enabled = "fcitx5";
+        fcitx5 = {
+          addons = with pkgs; [
+            fcitx5-chinese-addons
+            fcitx5-pinyin-zhwiki
+          ];
+          settings = {
+            globalOptions = {
+              "Hotkey/PrevPage"."1" = "comma";
+              "Hotkey/NextPage"."1" = "period";
+            };
+            inputMethod = {
+              "Groups/0".DefaultIM = "pinyin";
+              "Groups/0/Items/1".Name = "pinyin";
+            };
+            addons = {
+              classicui.globalSection = {
+                Font = "\"Sarasa UI SC 11\"";
+                MenuFont = "\"Sarasa UI SC 11\"";
+                TrayFont = "\"Sarasa UI SC 11\"";
+                Theme = "catppuccin-${toLower flavour}";
+                PerScreenDPI = "True";
+                EnableFractionalScale = "False";
+              };
+              pinyin.globalSection = {
+                CloudPinyinEnabled = "True";
+                PinyinInPreedit = "True";
+                Prediction = "True";
+              };
+            };
           };
         };
-        # ignoreUserConfig = true;
       };
-    };
-    home-manager.users.${user} = {
-      systemd.user.services.fcitx5-daemon = {
-        Unit = {
-          Description = "Fcitx5 input method editor";
-          PartOf = ["graphical-session.target"];
-        };
-        Service.ExecStart = "${config.i18n.inputMethod.package}/bin/fcitx5";
-        Install.WantedBy = ["graphical-session.target"];
-      };
+
+      xdg.dataFile."fcitx5/themes".source = fcitx5Theme + /src;
     };
   };
 }
