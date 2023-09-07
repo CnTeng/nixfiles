d6e02cc47a2d2380d8ab1e93bf4436fbec11d7bc
CnTeng <me@snakepi.xyz>
2023-09-08 01:05:03 +0800

    refactor: update dae config

--- nixos/services/dae.nix
+++ nixos/services/dae.nix
@@ -8,72 +8,65 @@
 }:
 with lib; let
   cfg = config.services'.dae;
-
-  configFile = pkgs.writeTextFile {
-    name = "config.dae";
-    text = ''
-      global {
-        log_level: debug
-
-        wan_interface: auto
-        auto_config_kernel_parameter: true
-      }
-
-      node {
-        naive: 'http://127.0.0.1:1080'
-      }
-
-      dns {
-        upstream {
-          google: 'tcp+udp://dns.google.com:53'
-          ali: 'udp://dns.alidns.com:53'
-        }
-        routing {
-          request {
-            qname(geosite:category-ads-all) -> reject
-            qname(geosite:cn) -> ali
-            fallback: google
-          }
-          response {
-            upstream(google) -> accept
-            !qname(geosite:cn) && ip(geoip:private) -> google
-            fallback: accept
-          }
-        }
-      }
-
-      group {
-        proxy {
-          policy: fixed(0)
-        }
-      }
-
-      routing {
-        pname(NetworkManager, naive) -> must_direct
-
-        dip(geoip:private) -> direct
-        dip(geoip:cn) -> direct
-        dip(8.8.8.8, 8.8.4.4) -> direct
-        dip(223.5.5.5, 223.6.6.6) -> direct
-        dip(224.0.0.0/3, 'ff00::/8') -> direct
-
-        domain(geosite:category-ads) -> block
-        domain(geosite:cn) -> direct
-
-        fallback: proxy
-      }
-    '';
-  };
 in {
   options.services'.dae.enable = mkEnableOption "dae";
 
   config = mkIf cfg.enable {
-    environment.etc."dae/config.dae" = {
-      mode = "0600";
-      source = configFile;
-    };
+    services.dae = {
+      enable = true;
+      config = ''
+        global {
+          log_level: debug
 
-    services.dae.enable = true;
+          wan_interface: auto
+          auto_config_kernel_parameter: true
+        }
+
+        node {
+          naive: 'http://127.0.0.1:1080'
+        }
+
+        dns {
+          upstream {
+            google: 'tcp+udp://dns.google.com:53'
+            ali: 'udp://dns.alidns.com:53'
+          }
+          routing {
+            request {
+              qname(geosite:category-ads-all) -> reject
+              qname(geosite:cn) -> ali
+              fallback: google
+            }
+            response {
+              upstream(google) -> accept
+              !qname(geosite:cn) && ip(geoip:private) -> google
+              fallback: accept
+            }
+          }
+        }
+
+        group {
+          proxy {
+            policy: fixed(0)
+          }
+        }
+
+        routing {
+          pname(NetworkManager, naive) -> must_direct
+
+          dip(geoip:private) -> direct
+          dip(geoip:cn) -> direct
+          dip(8.8.8.8, 8.8.4.4) -> direct
+          dip(223.5.5.5, 223.6.6.6) -> direct
+          dip(224.0.0.0/3, 'ff00::/8') -> direct
+
+          domain(geosite:category-ads) -> block
+          domain(geosite:cn) -> direct
+
+          fallback: proxy
+        }
+      '';
+    };
 
     systemd.services.naiveproxy = let
       settings = {
