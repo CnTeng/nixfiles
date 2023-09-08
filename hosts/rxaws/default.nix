e8d6a2c547619b5b575fd8b7328ff69df75dcc61
CnTeng <me@snakepi.xyz>
2023-09-08 16:53:08 +0800

    feat: add fail2ban

--- hosts/rxaws/default.nix
+++ hosts/rxaws/default.nix
@@ -3,6 +3,7 @@
 
   services' = {
     caddy.enable = true;
+    fail2ban.enable = true;
     firewall.enable = true;
     naive.enable = true;
     openssh.enable = true;
---
+++ nixos/services/fail2ban.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.services'.fail2ban;
+in {
+  options.services'.fail2ban.enable = mkEnableOption "fail2ban";
+
+  config = mkIf cfg.enable {
+    services.fail2ban.enable = true;
+  };
+}
