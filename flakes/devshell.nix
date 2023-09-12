9e35b0e24cf8975f61c91de7b7de024389e918a0
CnTeng <me@snakepi.xyz>
2023-09-12 09:26:34 +0800

    feat: add terraform wrapper

--- flakes/devshell.nix
+++ flakes/devshell.nix
@@ -4,15 +4,46 @@
   perSystem = {
     config,
     pkgs,
+    lib,
     ...
   }: {
     devshells.default = {
-      commands = [
+      commands = let
+        terraform = pkgs.terraform.withPlugins (p: with p; [cloudflare sops hcloud]);
+      in [
         {
           name = "rebuild";
           category = "deploy";
           help = "nixos rebuild switch";
           command = "sudo nixos-rebuild switch --flake .#$@";
+        }
+        {
+          name = "terraform";
+          category = "deploy";
+          command = ''
+            sops --output-type json \
+                 --output terraform.tfstate \
+                 --decrypt tfstate.yaml
+
+            if [ $? -eq 0 ]; then
+                echo -e "sops: decryption successful\n"
+            else
+                echo -e "sops: decryption failed\n"
+            fi
+
+            ${lib.getExe terraform} $@
+
+            sops --input-type json \
+                 --output-type yaml \
+                 --output tfstate.yaml \
+                 --encrypt terraform.tfstate
+
+            if [ $? -eq 0 ]; then
+                echo -e "\nsops: encryption successful"
+            else
+                echo -e "\nsops: encryption failed"
+            fi
+          '';
         }
       ];
 
@@ -20,7 +51,6 @@
         colmena
         nvfetcher
         sops
-        (terraform.withPlugins (p: with p; [cloudflare sops hcloud]))
         terraform-ls
       ];
     };
