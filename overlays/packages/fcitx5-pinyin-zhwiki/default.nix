41f84eae44d6552a0072260abd49af18156d7aa6
CnTeng <me@snakepi.xyz>
2023-09-19 09:39:43 +0800

    fix: themes arm build broken

--- overlays/packages/fcitx5-pinyin-zhwiki/default.nix
+++ overlays/packages/fcitx5-pinyin-zhwiki/default.nix
@@ -11,6 +11,7 @@ stdenv.mkDerivation {
   installPhase = ''
     install -Dm644 $src $out/share/fcitx5/pinyin/dictionaries/zhwiki.dict
   '';
+
   meta = with lib; {
     description = "zhwiki dictionary for fcitx5-pinyin and rime";
     homepage = "https://github.com/felixonmars/fcitx5-pinyin-zhwiki";
--- overlays/themes/palette/default.nix
+++ overlays/themes/palette/default.nix
@@ -4,4 +4,9 @@
 }:
 stdenvNoCC.mkDerivation {
   inherit (source) src;
+
+  dontUnpack = true;
+  dontConfigure = true;
+  dontBuild = true;
+  dontInstall = true;
 }
