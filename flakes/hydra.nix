6ae58ba4f7f83973c252ff593d9ba5ad95ab22d8
CnTeng <me@snakepi.xyz>
2023-09-27 11:43:23 +0800

    feat: add more hydra jobs

--- flakes/hydra.nix
+++ flakes/hydra.nix
@@ -1,16 +1,19 @@
 {withSystem, ...}: {
-  flake = let
-    mkHydraJob = system: list:
-      with builtins;
-        listToAttrs (map (name: {
-            inherit name;
-            value = withSystem system ({pkgs, ...}: pkgs.${name});
-          })
-          list);
-  in {
-    hydraJobs = {
-      x86_64-linux = mkHydraJob "x86_64-linux" ["colmena" "nvfetcher" "caddy" "hyprland"];
-      aarch64-linux = mkHydraJob "aarch64-linux" ["colmena" "nvfetcher" "caddy"];
+  flake.hydraJobs = let
+    common = p: {
+      colmena = p.colmena;
+      nvfetcher = p.nvfetcher;
+      caddy = p.caddy;
+      naive = p.naive;
+      wsl-notify-send = p.wsl-notify-send;
     };
+    desktop = p: {
+      hyprland = p.hyprland;
+      waybar = p.waybar;
+      nemo = p.cinnamon.nemo-with-extensions;
+    };
+  in {
+    aarch64-linux = withSystem "aarch64-linux" ({pkgs, ...}: common pkgs);
+    x86_64-linux = withSystem "x86_64-linux" ({pkgs, ...}: common pkgs // desktop pkgs);
   };
 }
