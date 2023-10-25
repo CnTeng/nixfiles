ba7f873a37395fb84ac009aa407564f16772ec28
CnTeng <me@snakepi.xyz>
2023-10-25 11:16:39 +0800

    feat: add wps

---
+++ overlays/packages/ttf-wps-fonts/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  stdenv,
+  source,
+}:
+stdenv.mkDerivation {
+  inherit (source) pname version src;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/share/fonts/truetype
+    install -D *.{ttf,TTF} $out/share/fonts/truetype/
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Symbol fonts required by wps-office";
+    homepage = "https://github.com/BannedPatriot/ttf-wps-fonts";
+    license = licenses.unfree;
+    platforms = platforms.all;
+  };
+}
