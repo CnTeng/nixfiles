d3512a927e691850eeac7679a80a74603d2bb285
CnTeng <me@snakepi.xyz>
2023-11-20 16:38:38 +0800

    feat: use hm qt and fcitx5 config

--- nixos/desktop/profiles/input.nix
+++ nixos/desktop/profiles/input.nix
@@ -16,41 +16,35 @@ in {
     mkEnableOption "input method component";
 
   config = mkIf cfg.enable {
-    services.xserver = {
-      layout = "us";
-      xkbOptions = "ctrl:nocaps";
-      libinput.enable = true;
-    };
+    services.xserver.xkb.options = "ctrl:nocaps";
 
-    i18n.inputMethod = {
-      enabled = "fcitx5";
-      fcitx5 = {
-        addons = with pkgs; [
-          fcitx5-chinese-addons
-          fcitx5-pinyin-zhwiki
-        ];
-        settings = {
-          globalOptions = {
-            "Hotkey/PrevPage"."1" = "comma";
-            "Hotkey/NextPage"."1" = "period";
+    i18n.inputMethod.enabled = "fcitx5";
+    i18n.inputMethod.fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-chinese-addons
+        fcitx5-pinyin-zhwiki
+      ];
+      settings = {
+        globalOptions = {
+          "Hotkey/PrevPage"."1" = "comma";
+          "Hotkey/NextPage"."1" = "period";
+        };
+        inputMethod = {
+          "Groups/0".DefaultIM = "pinyin";
+          "Groups/0/Items/1".Name = "pinyin";
+        };
+        addons = {
+          classicui.globalSection = {
+            Font = "\"Sarasa UI SC 11\"";
+            MenuFont = "\"Sarasa UI SC 11\"";
+            TrayFont = "\"Sarasa UI SC 11\"";
+            Theme = "catppuccin-${toLower flavour}";
+            PerScreenDPI = "True";
+            EnableFractionalScale = "False";
           };
-          inputMethod = {
-            "Groups/0".DefaultIM = "pinyin";
-            "Groups/0/Items/1".Name = "pinyin";
-          };
-          addons = {
-            classicui.globalSection = {
-              Font = "\"Sarasa UI SC 11\"";
-              MenuFont = "\"Sarasa UI SC 11\"";
-              TrayFont = "\"Sarasa UI SC 11\"";
-              Theme = "catppuccin-${toLower flavour}";
-              PerScreenDPI = "True";
-              EnableFractionalScale = "False";
-            };
-            pinyin.globalSection = {
-              CloudPinyinEnabled = "True";
-              PinyinInPreedit = "True";
-            };
+          pinyin.globalSection = {
+            CloudPinyinEnabled = "True";
+            PinyinInPreedit = "True";
           };
         };
       };
@@ -59,14 +53,11 @@ in {
     environment.variables.GTK_IM_MODULE = lib.mkForce "wayland";
 
     home-manager.users.${user} = {
-      systemd.user.services.fcitx5-daemon = {
-        Unit = {
-          Description = "Fcitx5 input method editor";
-          PartOf = ["graphical-session.target"];
-        };
-        Service.ExecStart = getExe' config.i18n.inputMethod.package "fcitx5";
-        Install.WantedBy = ["graphical-session.target"];
-      };
+      i18n.inputMethod.enabled = "fcitx5";
+      i18n.inputMethod.fcitx5.addons = with pkgs; [
+        fcitx5-chinese-addons
+        fcitx5-pinyin-zhwiki
+      ];
 
       xdg.dataFile."fcitx5/themes".source = fcitx5Theme + /src;
     };
