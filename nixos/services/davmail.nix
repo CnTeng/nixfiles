c75046c8e4b6c874eeb6d1d8066eb0180bd92d1f
CnTeng <me@snakepi.xyz>
2023-12-04 23:53:40 +0800

    feat: remove protonmail account

--- nixos/services/davmail.nix
+++ nixos/services/davmail.nix
@@ -9,16 +9,7 @@ in {
     services.davmail = {
       enable = true;
       url = "https://outlook.office365.com/EWS/Exchange.asmx";
-      config = {
-        davmail = {
-          mode = "O365";
-          caldavPort = 11080;
-          imapPort = 11143;
-          ldapPort = 11389;
-          popPort = 11110;
-          smtpPort = 11025;
-        };
-      };
+      config.davmail.mode = "O365";
     };
   };
 }
