7c40b694a4facce12f6a3f1c7b09b7dd370d5f64
CnTeng <me@snakepi.xyz>
2023-12-11 09:12:43 +0800

    fix: caddy sha256

--- overlays/overrides/caddy/default.nix
+++ overlays/overrides/caddy/default.nix
@@ -2,7 +2,7 @@ prev:
 prev.caddy.override {
   buildGoModule = args:
     prev.buildGoModule (args // {
-      vendorHash = "sha256-AVaKjaWtbcBKgZIlTAi5wiTd2XzqdIYWtXxFGahcQEE=";
+      vendorHash = "sha256-LB66Qug8cTp0+7Q5rU+yCkQyR/V4+0Mmw2ZjEDo5qy0=";
       patches = [ ./caddy-with-plugins.patch ];
     });
 }
