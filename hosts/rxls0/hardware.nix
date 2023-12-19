dff235e8dd90292da9be982ec76dd860820f1ca8
CnTeng <me@snakepi.xyz>
2023-12-19 23:31:54 +0800

    feat: use zram generator

--- hosts/rxls0/hardware.nix
+++ hosts/rxls0/hardware.nix
@@ -2,4 +2,9 @@
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
 
   boot.loader.grub.device = lib.mkForce "/dev/nvme0n1";
+
+  services.zram-generator = {
+    enable = true;
+    settings.zram0 = { };
+  };
 }
