efe76e33ee5b807d3aad2a5f8b9231527f97c387
CnTeng <me@snakepi.xyz>
2023-12-29 09:14:26 +0800

    fix: adapt catppuccin palette rewrite

--- nixos/core/colors.nix
+++ nixos/core/colors.nix
@@ -19,7 +19,7 @@ in {
   };
 
   config.core'.colors.palette = {
-    inherit (palette.${toLower cfg.flavour})
+    inherit (palette.${toLower cfg.flavour}.colors)
       rosewater flamingo pink mauve red maroon peach yellow green teal sky
       sapphire blue lavender text subtext1 subtext0 overlay2 overlay1 overlay0
       surface2 surface1 surface0 base mantle crust;
