3f7e20f20b429948d72e251796445ee96ab80e06
CnTeng <me@snakepi.xyz>
2024-01-11 15:30:37 +0800

    feat: add network to hardware

--- hosts/rxls0/hardware.nix
+++ hosts/rxls0/hardware.nix
@@ -1,5 +1,10 @@
 { modulesPath, lib, ... }: {
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
 
+  hardware' = {
+    network.enable = true;
+    optimise.enable = true;
+  };
+
   boot.loader.grub.device = lib.mkForce "/dev/nvme0n1";
 }
