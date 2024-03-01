b0c84b8b310e39d00adc66931171d68ec7911a17
CnTeng <rxsnakepi@gmail.com>
2024-03-01 22:48:53 +0800

    feat: disable calibre for rxhc0

--- hosts/rxhc0/default.nix
+++ hosts/rxhc0/default.nix
@@ -8,11 +8,9 @@
 
   services' = {
     caddy.enable = true;
-    calibre.enable = true;
     fail2ban.enable = true;
     miniflux.enable = true;
     ntfy.enable = true;
-    onedrive.enable = true;
     openssh.enable = true;
     vaultwarden.enable = true;
     restic.enable = true;
