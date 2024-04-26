7442e484ea9c9bb949875690dbf30ed9a13bb2fe
CnTeng <rxsnakepi@gmail.com>
2024-04-26 09:42:32 +0800

    chore(services): remove calibre service

--- hosts/hcax/default.nix
+++ hosts/hcax/default.nix
@@ -5,11 +5,9 @@
 
   services' = {
     caddy.enable = true;
-    # calibre.enable = true;
     fail2ban.enable = true;
     miniflux.enable = true;
     ntfy.enable = true;
-    # onedrive.enable = true;
     vaultwarden.enable = true;
     restic.enable = true;
     tuic.server.enable = true;
