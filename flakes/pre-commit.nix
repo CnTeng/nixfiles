8eecad81212fdb4cc14fc3d31a7c9e2d2d025142
CnTeng <rxsnakepi@gmail.com>
2024-04-26 09:45:57 +0800

    ci: remove trufflehog check

--- flakes/pre-commit.nix
+++ flakes/pre-commit.nix
@@ -2,28 +2,10 @@
 {
   imports = [ inputs.pre-commit.flakeModule ];
 
-  perSystem =
-    {
-      config,
-      lib,
-      pkgs,
-      ...
-    }:
-    {
-      pre-commit.settings.hooks = {
-        commitizen.enable = true;
-        treefmt.enable = true;
-        trufflehog = {
-          enable = true;
-          entry =
-            lib.getExe' pkgs.trufflehog "trufflehog"
-            + " git file://."
-            + " --no-update"
-            + " --since-commit HEAD"
-            + " --only-verified"
-            + " --fail";
-          pass_filenames = false;
-        };
-      };
+  perSystem = {
+    pre-commit.settings.hooks = {
+      commitizen.enable = true;
+      treefmt.enable = true;
     };
+  };
 }
