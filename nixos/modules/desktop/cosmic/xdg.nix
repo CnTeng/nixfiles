963e5f066d642962f4c699a86633708e36a538da
CnTeng <rxsnakepi@gmail.com>
2024-06-13 21:54:24 +0800

    chore: move xdg config to profiles

--- nixos/modules/desktop/cosmic/xdg.nix
+++ nixos/modules/desktop/cosmic/xdg.nix
@@ -6,6 +6,7 @@
 }:
 let
   cfg = config.cosmic.profiles.xdg;
+
 in
 {
   options.cosmic.profiles.xdg.enable = lib.mkEnableOption' { };
@@ -19,35 +20,8 @@ in
       wantedBy = lib.mkForce [ "graphical-session.target" ];
     };
 
-    home-manager.users.${user} =
-      { config, ... }:
-      let
-        onedrive = "${config.home.homeDirectory}/OneDrive";
-      in
-      {
-        xdg.enable = true;
-
-        xdg.userDirs = {
-          enable = true;
-          desktop = null;
-          documents = "${onedrive}/Documents";
-          music = null;
-          publicShare = null;
-          templates = null;
-          pictures = "${onedrive}/Pictures";
-          videos = "${onedrive}/Videos";
-        };
-
-        xdg.mimeApps.enable = true;
-      };
-
-    environment.persistence."/persist" = {
-      users.${user}.directories = [
-        "Downloads"
-        "Projects"
-        "OneDrive"
-        ".config/onedrive"
-      ];
+    home-manager.users.${user} = {
+      xdg.mimeApps.enable = true;
     };
   };
 }
--- nixos/profiles/core/home.nix
+++ nixos/profiles/core/home.nix
@@ -4,6 +4,9 @@
   user,
   ...
 }:
+let
+  inherit (config.users.users.${user}) home;
+in
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
 
@@ -14,5 +17,36 @@
 
   home-manager.users.${user} = {
     home.stateVersion = config.system.stateVersion;
+
+    xdg.enable = true;
+
+    xdg.userDirs = {
+      enable = true;
+      desktop = null;
+      documents = "${home}/Documents";
+      download = "${home}/Inbox";
+      music = null;
+      pictures = "${home}/Pictures";
+      publicShare = null;
+      templates = null;
+      videos = null;
+      extraConfig = {
+        XDG_ARCHIVES_DIR = "${home}/Archives";
+        XDG_PROJECTS_DIR = "${home}/Projects";
+      };
+    };
+  };
+
+  environment.persistence."/persist" = {
+    users.${user}.directories = [
+      "Archives"
+      "Documents"
+      "Inbox"
+      "Pictures"
+      "Projects"
+
+      "OneDrive"
+      ".config/onedrive"
+    ];
   };
 }
