2b74dc01ccf2b9e8c1e0a4c42e179c70a94fa1be
CnTeng <rxsnakepi@gmail.com>
2024-06-28 10:51:39 +0800

    refactor: ssh config

--- nixos/profiles/core/ssh.nix
+++ nixos/profiles/core/ssh.nix
@@ -9,36 +9,39 @@ let
   cfg = config.programs.ssh;
   hosts = lib.filterAttrs (n: v: v.type == "remote") data.hosts;
 
-  mkSecret =
-    name: key:
-    (lib.nameValuePair name {
+  mkSecret = name: key: {
+    ${name} = {
       mode = "0444";
       inherit key;
-    });
+    };
+  };
 
-  mkTemplate =
-    name: content:
-    (lib.nameValuePair name {
+  mkTemplate = name: content: {
+    ${name} = {
       inherit content;
       mode = "0444";
-    });
+    };
+  };
 
-  mkHostsAttrs = f: hosts: lib.listToAttrs (lib.flatten (lib.mapAttrsToList f hosts));
+  mkKownHosts = host: keys: lib.concatMapStringsSep "\n" (key: "${host} ${key}") keys;
 
-  hostsSecrets = mkHostsAttrs (
+  hostsSecrets = lib.concatMapAttrs (
     host: hostData:
-    [
-      (mkSecret "ssh/${host}/ipv4" "hosts/${host}/ip/ipv4")
-      (mkSecret "ssh/${host}/host_rsa_key.pub" "hosts/${host}/host_rsa_key/public_key")
-      (mkSecret "ssh/${host}/host_ed25519_key.pub" "hosts/${host}/host_ed25519_key/public_key")
-    ]
-    ++ (lib.optionals hostData.initrd_ssh [
-      (mkSecret "ssh/${host}/initrd_host_rsa_key.pub" "hosts/${host}/initrd_host_rsa_key/public_key")
-      (mkSecret "ssh/${host}/initrd_host_ed25519_key.pub" "hosts/${host}/initrd_host_ed25519_key/public_key")
-    ])
+    lib.mergeAttrsList (
+      [
+        (mkSecret "ssh/${host}/ipv4" "hosts/${host}/ip/ipv4")
+        (mkSecret "ssh/${host}/host_rsa_key.pub" "hosts/${host}/host_rsa_key/public_key")
+        (mkSecret "ssh/${host}/host_ed25519_key.pub" "hosts/${host}/host_ed25519_key/public_key")
+      ]
+      ++ (lib.optionals hostData.initrd_ssh [
+        (mkSecret "ssh/${host}/initrd_rsa_key.pub" "hosts/${host}/initrd_rsa_key/public_key")
+        (mkSecret "ssh/${host}/initrd_ed25519_key.pub" "hosts/${host}/initrd_ed25519_key/public_key")
+      ])
+    )
   ) hosts;
 
-  hostsConfig = mkHostsAttrs (host: hostData: [
+  hostsConfig = lib.concatMapAttrs (
+    host: _:
     (mkTemplate "ssh/${host}/config" ''
       Host ${host}
         HostName ${config.sops.placeholder."ssh/${host}/ipv4"}
@@ -46,35 +49,23 @@ let
         IdentityFile /home/${user}/.ssh/id_ed25519_sk_rk_ybk5@nixos
         IdentityFile /home/${user}/.ssh/id_ed25519_sk_rk_ybk5c@nixos
     '')
-  ]) hosts;
+  ) hosts;
 
-  knownHosts = lib.listToAttrs [
-    (mkTemplate "ssh/known_hosts" (
-      lib.concatStringsSep "\n" (
-        lib.mapAttrsToList (
-          (
-            host: hostData:
-            ''
-              ${config.sops.placeholder."ssh/${host}/ipv4"} ${
-                config.sops.placeholder."ssh/${host}/host_rsa_key.pub"
-              }
-              ${config.sops.placeholder."ssh/${host}/ipv4"} ${
-                config.sops.placeholder."ssh/${host}/host_ed25519_key.pub"
-              }
-            ''
-            + (lib.optionalString hostData.initrd_ssh ''
-              ${config.sops.placeholder."ssh/${host}/ipv4"} ${
-                config.sops.placeholder."ssh/${host}/initrd_host_rsa_key.pub"
-              }
-              ${config.sops.placeholder."ssh/${host}/ipv4"} ${
-                config.sops.placeholder."ssh/${host}/initrd_host_ed25519_key.pub"
-              }
-            '')
-          )
-        ) hosts
-      )
-    ))
-  ];
+  kownHostsList = lib.mapAttrsToList (
+    host: hostData:
+    mkKownHosts config.sops.placeholder."ssh/${host}/ipv4" (
+      [
+        config.sops.placeholder."ssh/${host}/host_rsa_key.pub"
+        config.sops.placeholder."ssh/${host}/host_ed25519_key.pub"
+      ]
+      ++ lib.optionals hostData.initrd_ssh [
+        config.sops.placeholder."ssh/${host}/initrd_rsa_key.pub"
+        config.sops.placeholder."ssh/${host}/initrd_ed25519_key.pub"
+      ]
+    )
+  ) hosts;
+
+  knownHosts = mkTemplate "ssh/known_hosts" (lib.concatStringsSep "\n" kownHostsList);
 
 in
 {
