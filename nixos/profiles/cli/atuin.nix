fd313b08b5168f5461d554b62b217d759eeb10f4
CnTeng <rxsnakepi@gmail.com>
2024-08-10 14:37:06 +0800

    feat: change atuin keymap cursor

--- nixos/profiles/cli/atuin.nix
+++ nixos/profiles/cli/atuin.nix
@@ -5,8 +5,14 @@
       enable = true;
       settings = {
         sync_address = "https://atuin.snakepi.xyz";
+        workspaces = true;
         inline_height = 30;
         keymap_mode = "vim-normal";
+        keymap_cursor = {
+          emacs = "blink-bar";
+          vim_insert = "blink-bar";
+          vim_normal = "steady-block";
+        };
         sync.records = true;
       };
     };
