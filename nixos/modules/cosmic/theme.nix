5872c32e6325630f8ca10ec9f14ebd6439ab3b27
CnTeng <rxsnakepi@gmail.com>
2024-08-10 14:58:23 +0800

    chore: remove useless gtk config

--- nixos/modules/cosmic/theme.nix
+++ nixos/modules/cosmic/theme.nix
@@ -12,30 +12,27 @@ in
   options.cosmic.profiles.theme.enable = lib.mkEnableOption' { };
 
   config = lib.mkIf cfg.enable {
-    home-manager.users.${user} = {
-      qt = {
-        enable = true;
-        style.name = "adwaita-dark";
-      };
+    home-manager.users.${user} =
+      { config, ... }:
+      {
+        qt = {
+          enable = true;
+          style.name = "adwaita-dark";
+        };
 
-      gtk = {
-        enable = true;
-        font.name = "NotoSans Nerd Font";
-        iconTheme = {
-          package = pkgs.papirus-icon-theme;
-          name = "Papirus-Dark";
-        };
-        theme = {
-          package = pkgs.adw-gtk3;
-          name = "adw-gtk3-dark";
-        };
-        gtk3.extraConfig = {
-          gtk-application-prefer-dark-theme = true;
-        };
-        gtk4.extraConfig = {
-          gtk-application-prefer-dark-theme = true;
+        gtk = {
+          enable = true;
+          font.name = "NotoSans Nerd Font";
+          iconTheme = {
+            package = pkgs.papirus-icon-theme;
+            name = "Papirus-Dark";
+          };
+          theme = {
+            package = pkgs.adw-gtk3;
+            name = "adw-gtk3-dark";
+          };
+          gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
         };
       };
-    };
   };
 }
