d074bb8f3580572f9aeebcbc17bc70071a0b49b4
CnTeng <rxsnakepi@gmail.com>
2024-08-13 09:27:57 +0800

    feat: use new kitty keymaps

--- nixos/modules/gui/kitty.nix
+++ nixos/modules/gui/kitty.nix
@@ -7,7 +7,7 @@
 }:
 let
   cfg = config.gui'.kitty;
-  inherit (pkgs.vimPlugins) smart-splits-nvim;
+  inherit (pkgs.vimPlugins) smart-splits-nvim kitty-scrollback-nvim;
 in
 {
   options.gui'.kitty.enable = lib.mkEnableOption "kitty";
@@ -23,6 +23,11 @@ in
         };
         "kitty/relative_resize.py" = {
           source = "${smart-splits-nvim}/kitty/relative_resize.py";
+          executable = true;
+        };
+
+        "kitty/kitty_scrollback_nvim.py" = {
+          source = "${kitty-scrollback-nvim}/python/kitty_scrollback_nvim.py";
           executable = true;
         };
       };
@@ -51,62 +56,68 @@ in
             listen_on = "unix:@mykitty";
           };
         keybindings = {
-          "kitty_mod+enter" = "launch --location=split";
-          "kitty_mod+q" = "close_window";
+          "ctrl+enter" = "launch --location=split";
+          "ctrl+q" = "close_window";
+          "ctrl+w>q" = "close_window";
 
-          "kitty_mod+j" = "neighboring_window down";
-          "kitty_mod+k" = "neighboring_window up";
-          "kitty_mod+h" = "neighboring_window left";
-          "kitty_mod+l" = "neighboring_window right";
+          "ctrl+j" = "neighboring_window down";
+          "ctrl+k" = "neighboring_window up";
+          "ctrl+h" = "neighboring_window left";
+          "ctrl+l" = "neighboring_window right";
 
-          "ctrl+alt+j" = "move_window down";
-          "ctrl+alt+k" = "move_window up";
-          "ctrl+alt+h" = "move_window left";
-          "ctrl+alt+l" = "move_window right";
+          "ctrl+w>j" = "move_window down";
+          "ctrl+w>k" = "move_window up";
+          "ctrl+w>h" = "move_window left";
+          "ctrl+w>l" = "move_window right";
 
-          "kitty_mod+w" = "focus_visible_window";
+          "kitty_mod+h" = "kitten kitty_scrollback_nvim.py";
+          "kitty_mod+g" = "kitten kitty_scrollback_nvim.py --config ksb_builtin_last_cmd_output";
 
-          "ctrl+alt+s" = "toggle_layout stack";
-          "ctrl+alt+v" = "layout_action rotate";
+          "ctrl+w>u" = "scroll_page_up";
+          "ctrl+w>d" = "scroll_page_down";
 
-          "kitty_mod+1" = "goto_tab 1";
-          "kitty_mod+2" = "goto_tab 2";
-          "kitty_mod+3" = "goto_tab 3";
-          "kitty_mod+4" = "goto_tab 4";
-          "kitty_mod+5" = "goto_tab 5";
-          "kitty_mod+6" = "goto_tab 6";
-          "kitty_mod+7" = "goto_tab 7";
-          "kitty_mod+8" = "goto_tab 8";
-          "kitty_mod+9" = "goto_tab 9";
-          "kitty_mod+0" = "goto_tab 10";
+          "ctrl+w>p" = "focus_visible_window";
+          "kitty_mod+s" = "toggle_layout stack";
+
+          "ctrl+w>o" = "layout_action rotate";
+          "ctrl+o" = "layout_action rotate";
+
+          "ctrl+w>1" = "goto_tab 1";
+          "ctrl+w>2" = "goto_tab 2";
+          "ctrl+w>3" = "goto_tab 3";
+          "ctrl+w>4" = "goto_tab 4";
+          "ctrl+w>5" = "goto_tab 5";
+          "ctrl+w>6" = "goto_tab 6";
+          "ctrl+w>7" = "goto_tab 7";
+          "ctrl+w>8" = "goto_tab 8";
+          "ctrl+w>9" = "goto_tab 9";
+          "ctrl+w>0" = "goto_tab 10";
         };
         extraConfig = ''
           symbol_map U+4E00–U+9FFF Noto Sans Mono CJK SC
 
           modify_font underline_position +3
           modify_font underline_thickness 150%
-
           modify_font cell_height 120%
 
-          map ctrl+j neighboring_window down
-          map ctrl+k neighboring_window up
-          map ctrl+h neighboring_window left
-          map ctrl+l neighboring_window right
+          map --new-mode resize ctrl+w>r
+
+          map --mode resize j kitten relative_resize.py down  3
+          map --mode resize k kitten relative_resize.py up    3
+          map --mode resize h kitten relative_resize.py left  3
+          map --mode resize l kitten relative_resize.py right 3
+
+          map --mode resize esc pop_keyboard_mode
+          map --mode resize ctrl+[ pop_keyboard_mode
+
+          map --when-focus-on var:IS_NVIM ctrl+q
 
           map --when-focus-on var:IS_NVIM ctrl+j
           map --when-focus-on var:IS_NVIM ctrl+k
           map --when-focus-on var:IS_NVIM ctrl+h
           map --when-focus-on var:IS_NVIM ctrl+l
 
-          map alt+j kitten relative_resize.py down  3
-          map alt+k kitten relative_resize.py up    3
-          map alt+h kitten relative_resize.py left  3
-          map alt+l kitten relative_resize.py right 3
-
-          map --when-focus-on var:IS_NVIM alt+j
-          map --when-focus-on var:IS_NVIM alt+k
-          map --when-focus-on var:IS_NVIM alt+h
-          map --when-focus-on var:IS_NVIM alt+l
+          map --when-focus-on var:IS_NVIM ctrl+w
         '';
       };
     };
