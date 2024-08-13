81ac64fe81dcb5dbc1788e476c29a1849ee7386f
CnTeng <rxsnakepi@gmail.com>
2024-08-13 09:25:05 +0800

    chore: remove fcitx5 global settings

--- nixos/modules/cosmic/input.nix
+++ nixos/modules/cosmic/input.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  user,
   ...
 }:
 let
@@ -26,36 +27,13 @@ in
         fcitx5-pinyin-moegirl
         fcitx5-pinyin-zhwiki
       ];
-      settings = {
-        globalOptions = {
-          "Hotkey/PrevPage"."1" = "comma";
-          "Hotkey/NextPage"."1" = "period";
-        };
-        inputMethod = {
-          "Groups/0" = {
-            Name = "Default";
-            "Default Layout" = "us";
-            DefaultIM = "keyboard-us";
-          };
-          "Groups/0/Items/0".Name = "keyboard-us";
-          "Groups/0/Items/1".Name = "pinyin";
-          GroupOrder."0" = "Default";
-        };
-        addons = {
-          classicui.globalSection = {
-            Font = ''"Noto Sans Mono CJK SC 11"'';
-            MenuFont = ''"Noto Sans Mono CJK SC 11"'';
-            TrayFont = ''"Noto Sans Mono CJK SC 11"'';
-            UseDarkTheme = "True";
-            EnableFractionalScale = "False";
-          };
-          pinyin.globalSection = {
-            CloudPinyinEnabled = "True";
-            PinyinInPreedit = "True";
-          };
-        };
-      };
-      ignoreUserConfig = true;
+    };
+
+    environment.persistence."/persist" = {
+      users.${user}.directories = [
+        ".config/fcitx"
+        ".config/fcitx5"
+      ];
     };
   };
 }
