0b26e3cb37b8fa6feb434b3130a0da0943f08183
CnTeng <rxsnakepi@gmail.com>
2024-08-13 09:27:38 +0800

    chore: remove atuin vi keybindings

--- nixos/profiles/cli/atuin.nix
+++ nixos/profiles/cli/atuin.nix
@@ -7,12 +7,6 @@
         sync_address = "https://atuin.snakepi.xyz";
         workspaces = true;
         inline_height = 30;
-        keymap_mode = "vim-normal";
-        keymap_cursor = {
-          emacs = "blink-bar";
-          vim_insert = "blink-bar";
-          vim_normal = "steady-block";
-        };
         sync.records = true;
       };
     };
