dd1f6de13391132763f66d0b9042001eadb16ee5
CnTeng <rxsnakepi@gmail.com>
2024-08-18 18:21:19 +0800

    fix: remove boot-counting

--- hosts/rxtp/hardware.nix
+++ hosts/rxtp/hardware.nix
@@ -20,10 +20,7 @@
 
   boot.initrd.systemd.enable = true;
 
-  boot.loader.systemd-boot = {
-    enable = true;
-    bootCounting.enable = true;
-  };
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   hardware.cpu.amd.updateMicrocode = true;
