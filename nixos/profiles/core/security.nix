698e45892198320d44db0337a1dd73e4ccdf7d94
CnTeng <rxsnakepi@gmail.com>
2024-08-20 08:47:14 +0800

    chore: drop pam_rssh

--- nixos/profiles/core/security.nix
+++ nixos/profiles/core/security.nix
@@ -1,25 +1,9 @@
-{
-  config,
-  pkgs,
-  user,
-  ...
-}:
+{ config, ... }:
 {
   sops.secrets.yubikey = {
     sopsFile = ./secrets.yaml;
     mode = "0444";
   };
-
-  security.pam.services.sudo =
-    { config, ... }:
-    {
-      rules.auth.rssh = {
-        order = config.rules.auth.unix.order - 10;
-        control = "sufficient";
-        modulePath = "${pkgs.pam_rssh}/lib/libpam_rssh.so";
-        settings.auth_key_file = "/etc/ssh/authorized_keys.d/${user}";
-      };
-    };
 
   security.pam.u2f = {
     enable = true;
@@ -30,8 +14,4 @@
       cue = true;
     };
   };
-
-  security.sudo.extraConfig = ''
-    Defaults env_keep+=SSH_AUTH_SOCK
-  '';
 }
