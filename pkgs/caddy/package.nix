9a65529c347d2cd65747ebfaf1cf50dbd61a3fde
CnTeng <rxsnakepi@gmail.com>
2024-11-14 15:02:12 +0800

    chore: drop caddy-with-plugins.diff

--- pkgs/caddy/package.nix
+++ pkgs/caddy/package.nix
@@ -1,12 +1,34 @@
-{ prev }:
-prev.caddy.override {
-  buildGoModule =
-    args:
-    prev.buildGoModule (
-      args
-      // {
-        vendorHash = "sha256-/Vj8Yn34c81W1KZoCogfG3Z3fzmJnjvubxq3nIqHxm4=";
-        patches = [ ./caddy-with-plugins.diff ];
-      }
-    );
-}
+{ prev, lib }:
+let
+  plugins = [ "github.com/caddy-dns/cloudflare" ];
+  pluginNames = map (plugin: builtins.elemAt (lib.splitString "@" plugin) 0) plugins;
+in
+prev.caddy.overrideAttrs (old: {
+  vendorHash = "sha256-7hLCPVYlk9zY7GUelWymDKjdInMjhQn1JhLQ3af+57A=";
+
+  prePatch = ''
+    # Add modules to main.go
+    for plugin in ${toString pluginNames}; do
+      sed -i "/plug in Caddy modules here/a _ \"$plugin\"" cmd/caddy/main.go
+    done
+
+    # Outside go-modules derivation, copy go.mod and go.sum
+    [ -z "$goModules" ] || cp "$goModules/go.mod" "$goModules/go.sum" .
+  '';
+
+  preBuild = ''
+    # In go-modules derivation, fetch the plugins
+    [ -n "$goModules" ] || {
+      for plugin in ${toString plugins}; do
+        go get $plugin
+      done
+      go mod tidy
+    }
+  '';
+
+  modPostBuild = ''
+    # Once the modules are vendorized, also save go.mod and go.sum
+    cp go.mod go.sum vendor
+  '';
+
+})
