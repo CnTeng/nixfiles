eccfd81e808801cf8ec4b5caeb5ae6b2d0a19912
CnTeng <rxsnakepi@gmail.com>
2025-01-07 08:51:18 +0800

    fix: caddy build

--- nixos/modules/services/caddy.nix
+++ nixos/modules/services/caddy.nix
@@ -20,7 +20,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-WGV/Ve7hbVry5ugSmTYWDihoC9i+D3Ct15UKgdpYc9U=";
+        hash = "sha256-JoujVXRXjKUam1Ej3/zKVvF0nX97dUizmISjy3M3Kr8=";
       };
     };
 
