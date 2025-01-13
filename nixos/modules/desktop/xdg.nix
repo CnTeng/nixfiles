cfea44cf98d4077fe01e217441a0c2e988bae631
CnTeng <rxsnakepi@gmail.com>
2025-01-13 11:34:54 +0800

    chore: clean xdg code

--- nixos/modules/desktop/xdg.nix
+++ nixos/modules/desktop/xdg.nix
@@ -6,37 +6,20 @@
 }:
 let
   cfg = config.desktop'.xdg;
-  inherit (config.users.users.${user}) home;
 in
 {
   options.desktop'.xdg.enable = lib.mkEnableOption' { };
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${user} = {
-      xdg.userDirs = {
-        enable = true;
-        desktop = null;
-        documents = "${home}/Documents";
-        download = "${home}/Inbox";
-        music = null;
-        pictures = "${home}/Pictures";
-        publicShare = null;
-        templates = null;
-        videos = null;
-        extraConfig = {
-          XDG_ARCHIVES_DIR = "${home}/Archives";
-          XDG_PROJECTS_DIR = "${home}/Projects";
-        };
-      };
-
+      xdg.userDirs.enable = true;
       xdg.mimeApps.enable = true;
     };
 
     environment.persistence."/persist" = {
       users.${user}.directories = [
-        "Archives"
         "Documents"
-        "Inbox"
+        "Downloads"
         "Pictures"
         "Projects"
       ];
