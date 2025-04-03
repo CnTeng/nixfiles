ff8768796e95fb3d111964029ecd4f9d5f926f4e
CnTeng <rxsnakepi@gmail.com>
2025-04-03 14:39:57 +0800

    chore: update sops config

--- nixos/profiles/core/sops.nix
+++ nixos/profiles/core/sops.nix
@@ -1,14 +1,16 @@
-{ inputs, user, ... }:
+{
+  inputs,
+  config,
+  user,
+  ...
+}:
 {
   imports = [ inputs.sops-nix.nixosModules.default ];
 
   sops = {
     defaultSopsFile = ../../../infra/outputs/secrets.yaml;
-    age = {
-      sshKeyPaths = [ ];
-      keyFile = "/var/lib/sops-nix/key";
-    };
-    gnupg.sshKeyPaths = [ ];
+    environment.HOME = config.users.users.${user}.home;
+    age.keyFile = "/var/lib/sops-nix/key";
   };
 
   environment.persistence."/persist" = {
