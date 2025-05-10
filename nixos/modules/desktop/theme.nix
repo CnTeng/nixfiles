33b1cb6dbe6288e87e0181254d9fa103a7e6620b
CnTeng <rxsnakepi@gmail.com>
2025-05-10 11:41:48 +0800

    fix: gtk dark theme

--- nixos/modules/desktop/theme.nix
+++ nixos/modules/desktop/theme.nix
@@ -41,6 +41,12 @@ in
           };
           gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
         };
+
+        dconf.settings = {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+        };
       };
   };
 }
