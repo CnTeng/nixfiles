950969b958754e80f3cc7022ed7c3fb2c5256d4c
CnTeng <rxsnakepi@gmail.com>
2025-05-10 11:42:10 +0800

    chore: change ssh hosts config

--- nixos/profiles/core/ssh.nix
+++ nixos/profiles/core/ssh.nix
@@ -9,7 +9,7 @@
       addKeysToAgent = "yes";
       includes = [ "config.d/*.conf" ];
       matchBlocks = {
-        "*.snakepi.xyz" = {
+        "*" = {
           inherit user;
           identityFile = [
             "~/.ssh/id_ed25519_sk_rk_ybk5@nixos"
