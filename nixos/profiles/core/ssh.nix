a85d149274efd04d1e5c608b496b73cce1dad256
CnTeng <rxsnakepi@gmail.com>
2025-05-25 23:25:16 +0800

    feat: add id_ed25519 to identity file list

--- nixos/profiles/core/ssh.nix
+++ nixos/profiles/core/ssh.nix
@@ -8,14 +8,13 @@
       forwardAgent = true;
       addKeysToAgent = "yes";
       includes = [ "config.d/*.conf" ];
-      matchBlocks = {
-        "*" = {
-          inherit user;
-          identityFile = [
-            "~/.ssh/id_ed25519_sk_rk_ybk5@nixos"
-            "~/.ssh/id_ed25519_sk_rk_ybk5c@nixos"
-          ];
-        };
+      matchBlocks."*" = {
+        inherit user;
+        identityFile = [
+          "~/.ssh/id_ed25519"
+          "~/.ssh/id_ed25519_sk_rk_ybk5@nixos"
+          "~/.ssh/id_ed25519_sk_rk_ybk5c@nixos"
+        ];
       };
     };
   };
