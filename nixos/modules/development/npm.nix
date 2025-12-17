e4b3b828e3a294669bef7360b61665c364f83fe8
CnTeng <rxsnakepi@gmail.com>
2025-12-17 23:13:14 +0800

    feat: update npm config

--- nixos/modules/development/npm.nix
+++ nixos/modules/development/npm.nix
@@ -6,14 +6,14 @@ in
   options.development'.npm.enable = lib.mkEnableOption "";
 
   config = lib.mkIf cfg.enable {
-    programs.npm = {
+    hm'.programs.npm = {
       enable = true;
-      npmrc = ''
-        prefix=''${XDG_DATA_HOME}/npm
-        cache=''${XDG_CACHE_HOME}/npm
-        init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
-        logs-dir=''${XDG_STATE_HOME}/npm/logs
-      '';
+      settings = {
+        prefix = "\${XDG_DATA_HOME}/npm";
+        cache = "\${XDG_CACHE_HOME}/npm";
+        init-module = "\${XDG_CONFIG_HOME}/npm/config/npm-init.js";
+        logs-dir = "\${XDG_STATE_HOME}/npm/logs";
+      };
     };
 
     preservation'.user.directories = [
