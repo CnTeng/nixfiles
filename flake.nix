4a690a50b6bd9d57048e078699dfc790ff31b76e
CnTeng <rxsnakepi@gmail.com>
2025-12-31 20:23:05 +0800

    bump: lanzaboote to v1.0.0

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.rust-overlay.follows = "rust-overlay";
     };
