36f6c0f2a436584c2cb84ddeecaca5f31d922437
CnTeng <rxsnakepi@gmail.com>
2026-01-24 21:25:55 +0800

    feat: update desktop theme config

--- nixos/modules/desktop/theme.nix
+++ nixos/modules/desktop/theme.nix
@@ -11,36 +11,33 @@ in
   options.desktop'.theme.enable = lib.mkEnableOption "";
 
   config = lib.mkIf cfg.enable {
-    hm' =
-      { config, ... }:
-      {
-        home.pointerCursor = {
-          package = pkgs.adwaita-icon-theme;
-          name = "Adwaita";
-          size = 24;
-          gtk.enable = true;
-          dotIcons.enable = false;
-        };
+    hm' = {
+      home.pointerCursor = {
+        package = pkgs.adwaita-icon-theme;
+        name = "Adwaita";
+        size = 24;
+        gtk.enable = true;
+        dotIcons.enable = false;
+      };
 
-        qt = {
-          enable = true;
-          platformTheme.name = "adwaita";
-          style.name = "adwaita-dark";
-        };
+      qt = {
+        enable = true;
+        platformTheme.name = "adwaita";
+        style.name = "adwaita-dark";
+      };
 
-        gtk = {
-          enable = true;
-          font.name = "Adwaita Sans";
-          iconTheme = {
-            package = pkgs.papirus-icon-theme;
-            name = "Papirus-Dark";
-          };
-          theme = {
-            package = pkgs.adw-gtk3;
-            name = "adw-gtk3-dark";
-          };
-          gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      gtk = {
+        enable = true;
+        font.name = "Adwaita Sans";
+        theme = {
+          package = pkgs.adw-gtk3;
+          name = "adw-gtk3-dark";
+        };
+        iconTheme = {
+          package = pkgs.papirus-icon-theme;
+          name = "Papirus-Dark";
         };
       };
+    };
   };
 }
