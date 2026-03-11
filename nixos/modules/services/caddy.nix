2d37e6232c681c6bf24ba87b76578e7fa7af4f08
CnTeng <rxsnakepi@gmail.com>
2026-03-11 19:57:08 +0800

    bump: caddy with plugins

--- nixos/modules/services/caddy.nix
+++ nixos/modules/services/caddy.nix
@@ -15,7 +15,7 @@ in
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-        hash = "sha256-mmkziFzEMBcdnCWCRiT3UyWPNbINbpd3KUJ0NMW632w=";
+        hash = "sha256-bL1cpMvDogD/pdVxGA8CAMEXazWpFDBiGBxG83SmXLA=";
       };
       globalConfig = ''
         admin off
