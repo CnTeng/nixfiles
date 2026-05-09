3202abf4b02c17d09550d464a14859b4f099cb3c
CnTeng <rxsnakepi@gmail.com>
2026-05-10 00:49:57 +0800

    chore: build silverbullet from source

---
+++ pkgs/silverbullet/package.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildNpmPackage,
+  buildGoModule,
+  replaceVars,
+}:
+
+let
+  version = "2.7.0";
+
+  src = fetchFromGitHub {
+    owner = "silverbulletmd";
+    repo = "silverbullet";
+    rev = version;
+    hash = "sha256-6Jpo7Nugais7KaFnkyzKttZDHcwgcFGMlVXa2gGcmqk=";
+  };
+
+  frontend = buildNpmPackage (finalAttrs: {
+    pname = "silverbullet-frontend";
+    inherit version src;
+
+    npmDepsHash = "sha256-cn7s7JK6JV9NF0w+gTU56Y3bnR0xKMzvNRlh5GIpuA8=";
+
+    patches = [
+      (replaceVars ./override-public-version.patch { inherit version; })
+    ];
+
+    postBuild = ''
+      npm run build:plug-compile
+      rm -f client_bundle/client_bundle
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp -r client_bundle public_version.ts $out/
+
+      runHook postInstall
+    '';
+  });
+in
+buildGoModule {
+  pname = "silverbullet";
+  inherit version src;
+
+  vendorHash = "sha256-SvMPyJbSVrj+lwXrNh2WEYNI41oqlzchFxCtXvIl4/4=";
+  subPackages = [ "." ];
+
+  preBuild = ''
+    cp -r ${frontend}/client_bundle .
+    cp ${frontend}/public_version.ts .
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    install -Dm755 "$GOPATH/bin/silverbullet" $out/bin/silverbullet
+
+    runHook postInstall
+  '';
+
+  meta = {
+    changelog = "https://github.com/silverbulletmd/silverbullet/blob/${version}/CHANGELOG.md";
+    description = "Open-source, self-hosted, offline-capable Personal Knowledge Management (PKM) web application";
+    homepage = "https://silverbullet.md";
+    license = lib.licenses.mit;
+    mainProgram = "silverbullet";
+    platforms = lib.platforms.unix;
+  };
+}
