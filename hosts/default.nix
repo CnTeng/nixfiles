29742f84312307070fa4d7860edf9e498feca4ce
CnTeng <rxsnakepi@gmail.com>
2026-06-24 00:55:30 +0800

    bump: stateVersion to 26.05

--- hosts/default.nix
+++ hosts/default.nix
@@ -22,7 +22,7 @@ let
             userName = "yufei";
             hostName = host;
             hostInfo = data.hosts.${host};
-            stateVersion = "25.11";
+            stateVersion = "26.05";
           };
         }
         self.nixosModules.default
